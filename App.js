d4447960309c0f8e41c2e19416ebdb24ae85e66b
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2021-10-01 09:21:23 +0530

    feat: learned basic components

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	Button,
+	TextInput,
+	Switch,
+	Image,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [text, setText] = useState("");
+	return (
+		<SafeAreaView style={styles.container}>
+			<StatusBar style="auto" />
+			<Image
+				blurRadius={0.9}
+				source={{ uri: "https://picsum.photos/200/400" }}
+				style={styles.pic1}
+			/>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "yellow",
+		alignItems: "center",
+		justifyContent: "center",
+		borderWidth: 5,
+		borderColor: "red",
+	},
+	pic1: {
+		width: 300,
+		height: 200,
+		borderWidth: 2,
+	},
 });
