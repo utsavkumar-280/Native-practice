16182d4fd8451152a5943f1a735898a81774d366
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2021-10-03 18:07:18 +0530

    feat: touchables

--- App.js
+++ App.js
@@ -11,12 +11,35 @@ import {
 	Alert,
 	Platform,
 	StatusBar,
+	TouchableWithoutFeedback,
+	TouchableOpacity,
+	TouchableHighlight,
+	TouchableNativeFeedback,
 } from "react-native";
 
 export default function App() {
 	const [text, setText] = useState("");
 	return (
 		<SafeAreaView style={styles.container}>
+			<TouchableHighlight onPress={() => alert("hello ji")}>
+				<Image source={require("./assets/favicon.png")} />
+			</TouchableHighlight>
+			<TouchableOpacity onPress={() => alert("hello ji")}>
+				<Image source={require("./assets/favicon.png")} />
+			</TouchableOpacity>
+			<TouchableWithoutFeedback onPress={() => alert("hello ji")}>
+				<Image source={require("./assets/favicon.png")} />
+			</TouchableWithoutFeedback>
+			<TouchableNativeFeedback>
+				<View
+					style={{
+						backgroundColor: "#d99089",
+						height: 50,
+						width: 300,
+						margin: 30,
+					}}
+				></View>
+			</TouchableNativeFeedback>
 			<Button
 				title="Click Me"
 				onPress={() =>
@@ -35,7 +58,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: "#fff",
 		paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-		// alignItems: "center",
-		// justifyContent: "center",
+		alignItems: "center",
+		justifyContent: "center",
 	},
 });
