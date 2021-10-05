8d07fc16dc6eb0a4540bd1fd26611a912302b51d
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2021-10-05 09:07:29 +0530

    feat: added View Screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import { View } from "react-native";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
-import { View } from "react-native";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
 	const [text, setText] = useState("");
-	return <ListingDetailsScreen />;
+	return <ViewImageScreen />;
 }
